82b53203ace2ebbce66910f1576cf38d6ae448b5
Ashutosh <ashutosh.akss@gmail.com>
2026-04-16 11:21:10 +0530

    feat: dev mode fix

---
+++ dev/config.ts
@@ -0,0 +1,23 @@
+export const styles = {
+    // grid: {
+    //   show: false,
+    //   horizontal: {
+    //     show: true,
+    //   },
+    //   vertical: {
+    //     show: true,
+    //   }
+    // },
+    candle: {
+      type: 'candle_solid',
+      bar: {
+        compareRule: 'previous_close',
+        // upColor: 'blue',
+        // downColor: 'yellow',
+        // noChangeColor: '#888888',
+      },
+    },
+      crosshair: {
+        show: true,  
+      }
+};
